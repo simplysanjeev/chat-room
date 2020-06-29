2141c415492d49de0f86542528c42d05daa968e2
darkrider2109 <sanjeevkumarv2.0@gmail.com>
2020-06-30 00:37:17 +0530

    add auth

--- config/passport-google-outh2-strategy.js
+++ config/passport-google-outh2-strategy.js
@@ -6,8 +6,8 @@ const User = require('../models/User');
 
 // tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
-    clientID: "1079242500188-uh9vmh0anjecm6v9jq7ml2dehcocgnfa.apps.googleusercontent.com",
-    clientSecret: "9Tdb5lOh6U6o3kXepS0BnUi2",
+    clientID: "<CLIENT_ID>",
+    clientSecret: "<CLIENT_SECRET>",
     callbackURL: "http://localhost:8000/users/auth/google/callback"
 },
 
